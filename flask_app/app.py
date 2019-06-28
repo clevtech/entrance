6771b8787d95252bbd45f9e3124daf5b210b9e2a
Bauyrzhan Ospan <dr.cleverest@gmail.com>
2019-06-28 20:35:44 +0600

    Last One

--- flask_app/app.py
+++ flask_app/app.py
@@ -91,4 +91,4 @@ def chechit():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=5000, debug=True) #, ssl_context=('cert.pem', 'key.pem'))
